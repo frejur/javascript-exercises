d7c35a0bd66febe919675420fee13702d40332b2
Fredrik Jureen <fredrik.jureen@gmail.com>
2022-11-06 23:55:31 +0100

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...idx_a) {
+    for (const idx of idx_a)
+        array = array.filter(a => a !== idx);
+    return array;
 };
 
 // Do not edit below this line

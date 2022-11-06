545accbc574e2d5e02c659cfe4836818f8cf9af0
Fredrik Jureen <fredrik.jureen@gmail.com>
2022-11-07 00:32:42 +0100

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(...bounds) {
+    if (bounds[0] < 0 || bounds[1] < 0 ||
+        typeof bounds[0] !== "number" ||
+        typeof bounds[1] !== "number")
+    {
+            return "ERROR";
+    }
+    bounds = bounds.slice(0,2).sort();
+    return (bounds[0] < bounds[1] ? bounds[1] + sumAll(bounds[0], --bounds[1]) : bounds[0]);
 };
 
 // Do not edit below this line
